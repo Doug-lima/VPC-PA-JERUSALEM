7b3053c1a16df4fe805851fa40427c35e156086d
Doug-lima <douglas.dasilvalima@outlook.com>
2023-03-02 23:45:31 -0300

    ALB adicionado, falta habilitar internet nas subnet privadas

--- main.tf
+++ main.tf
@@ -7,8 +7,6 @@ terraform {
   }
 }
 
-#####TESTE NOTEBOOK
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
@@ -17,7 +15,7 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "vpc_jerusalem" {
   cidr_block = "10.0.0.0/16"
-
+ # enable_dns_hostnames = true
   tags = {
     Name      = "VPC PA Jerusalem"
     Owner     = "Douglas"
@@ -202,6 +200,19 @@ resource "aws_security_group_rule" "app_server_sg_inbound_80" {
   security_group_id = aws_security_group.webserver.id ##Resource: aws_vpc_endpoint_security_group_association
 }
 
+
+# Criando Regra de entrada para liberar a porta 80 // Resource: aws_security_group_rule
+resource "aws_security_group_rule" "app_server_sg_inbound_443" {
+  description = "443 from anywhere"
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  # ipv6_cidr_blocks  = [aws_vpc.vpc_terraform.ipv6_cidr_block]
+  security_group_id = aws_security_group.webserver.id ##Resource: aws_vpc_endpoint_security_group_association
+}
+
 # Criando Regra de saída
 resource "aws_security_group_rule" "app_server_sg_outbound" {
   type        = "egress"
@@ -212,3 +223,112 @@ resource "aws_security_group_rule" "app_server_sg_outbound" {
   #ipv6_cidr_blocks  = [aws_vpc.vpc_terraform.ipv6_cidr_block]
   security_group_id = aws_security_group.webserver.id
 }
+
+#Criando grupo de segurança do elb
+resource "aws_security_group" "alb" {
+  name        = "alb"
+  description = "alb network traffic"
+  vpc_id      = aws_vpc.vpc_jerusalem.id
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.webserver.id]
+  }
+
+  tags = {
+    Name = "allow traffic"
+  }
+}
+
+#Configurando lauch_template / EC2 aws_launch_template
+
+resource "aws_launch_template" "launchtemplate1" {
+  name = "web"
+
+  image_id      = "ami-09e67e426f25ce0d7"
+  instance_type = "t3a.small"
+  #key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "VM CMS"
+    }
+  }
+
+  user_data = filebase64("${path.module}/ec2.userdata")
+}
+
+#Configurando load balancing / ELB (Elastic Load Balancing)
+
+resource "aws_lb" "alb1" {
+  name               = "alb1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet3.id]
+
+  enable_deletion_protection = false
+
+  /*
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+  */
+
+  tags = {
+    Environment = "Prod"
+  }
+}
+
+#Configurando target Group
+
+resource "aws_lb_target_group" "webserver" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc_jerusalem.id
+
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb1.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webserver.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "rule1" {
+  listener_arn = aws_lb_listener.front_end.arn
+  priority     = 99
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webserver.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
